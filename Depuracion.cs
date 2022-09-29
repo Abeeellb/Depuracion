d5d74635869e882abb9cd8e77af25390197b16b1
Abeeellb <114142543+Abeeellb@users.noreply.github.com>
2022-09-29 10:13:34 +0200

    Add files via upload

---
+++ Depuracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Depuracion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a hacer una operaciones de chill");
+            int x;
+            int y;
+            x = 9;
+            y = 11;
+            x = x + y;
+            x = x * 65;
+            Console.WriteLine("El resultado de las operaciones es " + x);
+        }
+    }
+}
---
+++ Depuracion2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Depuracion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a hacer una operaciones de chill");
+            int x;
+            int y;
+            int z;
+            x = 9;
+            y = 11;
+            z = 25;
+            z= x + y + z;
+            z= x * 65 - z;
+            Console.WriteLine("El resultado de las operaciones es " + z);
+        }
+    }
+}
